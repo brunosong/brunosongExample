d1f13c0e29ac606293b632e7544dbc5e609fbc93
brunosong <playprogramer@gmail.com>
2024-10-20 21:13:59 +0900

    create reactor operator test exam4

---
+++ spring-webflux/reactor/src/test/java/com/brunosong/exam/Operator4Test.java
@@ -0,0 +1,24 @@
+package com.brunosong.exam;
+
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+
+class Operator4Test {
+
+    private Operator4 operator = new Operator4();
+
+    @Test
+    void fluxDelayAndLimit() {
+        StepVerifier.create(operator.fluxDelayAndLimit())
+                .expectNext(1,2,3,4,5,6,7,8,9,10)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxSample() {
+        StepVerifier.create(operator.fluxSample())
+                .expectNextCount(100)
+                .verifyComplete();
+    }
+}
