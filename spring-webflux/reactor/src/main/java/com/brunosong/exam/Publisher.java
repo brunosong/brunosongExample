1af1dd8a51ff120f7e34fab5b054d73702e91fd7
brunosong <playprogramer@gmail.com>
2024-10-20 16:44:25 +0900

    create reactor test exam

--- spring-webflux/reactor/src/main/java/com/brunosong/exam/Publisher.java
+++ spring-webflux/reactor/src/main/java/com/brunosong/exam/Publisher.java
@@ -3,6 +3,8 @@ package com.brunosong.exam;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 public class Publisher {
 
     public Flux<Integer> startFlux() {
@@ -11,11 +13,22 @@ public class Publisher {
         return Flux.range(1,10).log();
     }
 
-    public Mono<?> startMono() {
-        // 단일값
-        //return Mono.just(1).log();
+    public Flux<String> startFlux2() {
+        return Flux.fromIterable(List.of("a","b","c")).log();
+    }
 
+    public Mono<?> startMono() {
         // 비어있는 값을 보낼 수 있다
         return Mono.empty().log();
+    }
+
+    public Mono<Integer> startMono2() {
+        // 단일값
+        return Mono.just(1).log();
+    }
+
+    public Mono<?> startMono3() {
+        // 에러
+        return Mono.error(new Exception("hello reactor")).log();
     }
 }
