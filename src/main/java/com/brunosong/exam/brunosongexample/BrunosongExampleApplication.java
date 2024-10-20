55bb94d3b71ec30d27904e1404f05f65e612ee19
brunosong <playprogramer@gmail.com>
2024-10-20 15:56:48 +0900

    init brunosongExam

---
+++ src/main/java/com/brunosong/exam/brunosongexample/BrunosongExampleApplication.java
@@ -0,0 +1,13 @@
+package com.brunosong.exam.brunosongexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunosongExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrunosongExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brunosong/exam/brunosongexample/BrunosongExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosong.exam.brunosongexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunosongExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
