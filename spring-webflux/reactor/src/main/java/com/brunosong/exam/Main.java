07f40c50048f8c8c6a34c77ca672495589ad3eec
brunosong <playprogramer@gmail.com>
2024-10-20 16:26:13 +0900

    Mono Exam

--- spring-webflux/reactor/src/main/java/com/brunosong/exam/Main.java
+++ spring-webflux/reactor/src/main/java/com/brunosong/exam/Main.java
@@ -6,8 +6,11 @@ public class Main {
 
         // 반드시 subscribe를 가져와서 처리 해줘야 한다.
         // 구조는 Reactor1.png 확인
-        publisher.startFlux()
-                .subscribe(System.out::println);
+//        publisher.startFlux()
+//                .subscribe(System.out::println);
+
+        publisher.startMono()
+                        .subscribe(System.out::println);
 
         System.out.println("Hello world!");
     }
--- spring-webflux/reactor/src/main/java/com/brunosong/exam/Publisher.java
+++ spring-webflux/reactor/src/main/java/com/brunosong/exam/Publisher.java
@@ -1,6 +1,7 @@
 package com.brunosong.exam;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class Publisher {
 
@@ -8,5 +9,13 @@ public class Publisher {
         // Flux.just(1,2,3,4,5)
         // Flux.fromIterable(List.of("a","b","c"))
         return Flux.range(1,10).log();
+    }
+
+    public Mono<?> startMono() {
+        // 단일값
+        //return Mono.just(1).log();
+
+        // 비어있는 값을 보낼 수 있다
+        return Mono.empty().log();
     }
 }
