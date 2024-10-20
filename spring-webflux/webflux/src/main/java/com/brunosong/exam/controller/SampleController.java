bc13e3424682bff8e0537cd3ba599a80c178c65e
brunosong <playprogramer@gmail.com>
2024-10-20 22:11:55 +0900

    create webflux basic sample

---
+++ spring-webflux/webflux/src/main/java/com/brunosong/exam/controller/SampleController.java
@@ -0,0 +1,21 @@
+package com.brunosong.exam.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/sample/hello")
+    public Mono<String> getHello() {
+
+        // reactor
+        // publisher <--> subscriber
+
+        return Mono.just("hello rest controller with webflux");
+    }
+
+
+
+}
