3c83fb8dc6d6edc7660e8f30977b7ca1136b582a
brunosong <playprogramer@gmail.com>
2025-01-11 14:41:26 +0900

    create reflection exam

---
+++ Java/Reflection/src/test/java/com/brunosong/javaexam/ReflectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosong.javaexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
