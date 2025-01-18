af3ab6e7fa16277435469ae18cb5d6034651ae1c
brunosong <playprogramer@gmail.com>
2025-01-18 18:47:00 +0900

    Component Scan

--- spring-basic/src/main/java/com/brunosong/springbasic/HelloController.java
+++ spring-basic/src/main/java/com/brunosong/springbasic/HelloController.java
@@ -1,11 +1,11 @@
 package com.brunosong.springbasic;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@Controller
+@RestController
 public class HelloController {
 
     private HelloService helloService;
--- spring-basic/src/main/java/com/brunosong/springbasic/SimpleHelloServiceImpl.java
+++ spring-basic/src/main/java/com/brunosong/springbasic/SimpleHelloServiceImpl.java
@@ -1,5 +1,8 @@
 package com.brunosong.springbasic;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleHelloServiceImpl implements HelloService {
     @Override
     public String sayHello(String name) {
