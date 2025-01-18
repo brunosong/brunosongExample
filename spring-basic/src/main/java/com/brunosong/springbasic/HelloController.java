8e49617fb28af15589d0f5057815e399eea8b778
brunosong <playprogramer@gmail.com>
2025-01-18 19:05:15 +0900

    Bean의 생명주기 메소드

--- spring-basic/src/main/java/com/brunosong/springbasic/HelloController.java
+++ spring-basic/src/main/java/com/brunosong/springbasic/HelloController.java
@@ -1,17 +1,21 @@
 package com.brunosong.springbasic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-public class HelloController {
+public class HelloController { //implements ApplicationContextAware {
 
     private HelloService helloService;
+    private ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
     }
 
     @GetMapping("/hello")
@@ -19,4 +23,10 @@ public class HelloController {
         // 컨트롤러에 중요한 역할은 값을 검증하는 것이다.
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
+//    @Override
+//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+//        System.out.println(applicationContext);
+//        this.applicationContext = applicationContext;
+//    }
 }
